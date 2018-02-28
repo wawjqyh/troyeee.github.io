47606ec698840766fdfa28b2c0942eee4a26beed
wawjqyh <1205639813@qq.com>
2018-02-28 15:00:01 +0800

    项目结构修改

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -13,28 +10,17 @@ app.set("env", "production");
 /**
  * 基础中间件
  */
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-
-/**
- * bodyParser用于解析客户端请求体中的内容,内部使用JSON编码处理,url编码处理以及对于文件的上传处理
- */
-app.use(bodyParser.json());
-//解析post请求
-app.use(bodyParser.urlencoded({extended: false}));
-//解析cookie
-app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'src/images', 'favicon.ico')));
 
 /**
  * 配置静态文件目录
  */
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/index.html", express.static(path.join(__dirname, "index.html")));
+app.use("/src", express.static(path.join(__dirname, 'src')));
 app.use("/docs", express.static(path.join(__dirname, 'docs')));
-
-/**
- * 在express 默认生成的代码中，变量名为logger（记录器）;在控制台中，显示req请求的信息。
- * 也就是在控制台输出logger
- */
-app.use(logger('dev'));
+app.use("/", function (req, res, next) {
+    res.redirect("/index.html");
+});
 
 /**
  * 错误处理
