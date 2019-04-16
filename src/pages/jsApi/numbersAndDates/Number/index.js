0b2990eb0de68e39c7f31c80b4b214b954dfad22
wawjqyh <1205639813@qq.com>
2019-04-16 13:40:10 +0800

    更新Math文档

--- src/pages/jsApi/numbersAndDates/Number/index.js
+++ src/pages/jsApi/numbersAndDates/Number/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Card from '@/components/card';
 
-const prefix = '/jsApi/fundamentalObjects/object/';
+const prefix = '/jsApi/numbersAndDates/Number/';
 
 class Index extends Component {
   render() {
